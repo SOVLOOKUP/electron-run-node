2be332d00471e5591099e0a6e0e31bd78f19bce2
fan xia <gonorth@qq.com>
2023-10-04 11:42:57 +0800

    use abs path

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import type { ExecaReturnValue, Options } from "execa"
 import { existsSync } from "fs"
-import { join } from "path"
+import { resolve as rp } from "path"
+import { isAbsolute, join } from "path"
 
 export const runNode = (argv: string[], options?: Options<"utf8">) => new Promise<ExecaReturnValue>(async (resolve, reject) => {
     const { execa } = await import("execa")
-    const m = argv.at(0)
-    if (m && existsSync(m)) {
-        const res = await execa(process.argv.at(0)!, [join(__dirname, "../loader.js"), ...argv], options)
-        resolve(res)
+    if (!isAbsolute(argv[0])) {
+        argv[0] = rp(argv[0])
+    }
+    if (existsSync(argv[0])) {
+        try {
+            resolve(await execa(process.argv.at(0)!, [join(__dirname, "../loader.js"), ...argv], options))
+        } catch (error) {
+            reject(error)
+        }
     } else {
         reject(new Error("module not found"))
     }
